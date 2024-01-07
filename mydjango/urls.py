4c01af4d2dd80987e4853c48f427bacc6263f5ec
Narayan1180 <narayanchandra952@gmail.com>
2024-01-08 00:50:21 +0530

    hello ji l

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+
 from django.conf.urls.static import static
 
 
@@ -32,6 +33,5 @@ urlpatterns = [
      
 ]
 
-urlpatterns = [
-    # ... your other URL patterns
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
