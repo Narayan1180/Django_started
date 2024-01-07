ee51a3fac1caf2756bf484657f531088b630469d
Narayan1180 <62800382+Narayan1180@users.noreply.github.com>
2024-01-07 16:24:42 +0530

    Update settings.py

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -91,16 +91,15 @@ WSGI_APPLICATION = 'mydjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'narayan',
-        'USER':'postgres',
-        'PASSWORD':'12345',
-        'HOST':'localhost'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
